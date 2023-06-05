7c4ee775a4867cc7516b39510b83bcf4b557d5c0
esaia <esaiagafrindashvili@gmail.com>
2023-06-05 13:39:33 +0400

    feat: add dropColumns in down method when migration heppens

--- database/migrations/2023_06_05_081909_add_google_id_to_users_table.php
+++ database/migrations/2023_06_05_081909_add_google_id_to_users_table.php
@@ -5,9 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
 
@@ -19,13 +16,12 @@ return new class () extends Migration {
 
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('google_id');
+            $table->dropColumn('img');
         });
     }
 };
