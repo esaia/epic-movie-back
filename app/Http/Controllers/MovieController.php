a15d13b41fd657cf2397eb3583a15f39b47b0c4a
esaia <esaia@gmail.com>
2023-06-23 17:30:54 +0400

    add return type

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CreateMovieRequest;
 use App\Http\Requests\UpdateMovieRequest;
 use App\Models\Movie;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-	public function index(Request $request)
+	public function index(Request $request): Collection
 	{
 		$searchQuery = $request->input('searchQuery');
 		$user = $request->user();
